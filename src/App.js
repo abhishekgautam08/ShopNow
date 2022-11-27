00f0658ef473e78ba2932177458c46054b4bf089
Abhishek <gautamabhishek0810@gmail.com>
2022-11-27 15:34:41 +0530

    final

--- src/App.js
+++ src/App.js
@@ -6,42 +6,49 @@ import AboutPage from "./Pages/AboutPage";
 import ContactPage from "./Pages/ContactPage";
 import Navbar from "./components/Header/Navbar";
 import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 function App() {
   return (
     <>
-      <Navbar />
-      <div
-        className="main-container"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            alignItems: "center",
-            overflowY: "auto",
-          }}
-        >
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/Home" element={<Home />} />
-            <Route exact path="/Products" element={<ProductsPage />} />
-            <Route exact path="/about" element={<AboutPage />} />
-            <Route exact path="/contact" element={<ContactPage />} />
-          </Routes>
-        </div>
-        <div
-          style={{
-            height: 150,
-          }}
-        >
-          <Footer />
-        </div>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <div
+            className="main-container"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <div
+              style={{
+                flex: 1,
+                display: "flex",
+                alignItems: "center",
+                overflowY: "auto",
+              }}
+            >
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/Home" element={<Home />} />
+                <Route exact path="/Products" element={<ProductsPage />} />
+                <Route exact path="/about" element={<AboutPage />} />
+                <Route exact path="/contact" element={<ContactPage />} />
+              </Routes>
+            </div>
+            <div
+              style={{
+                height: 150,
+              }}
+            >
+              <Footer />
+            </div>
+          </div>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
